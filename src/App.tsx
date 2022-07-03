a5a99209abecb2a311576fe896622bb2d810c32b
!Kpose <ganihujude@gmail.com>
2022-07-03 18:45:55 +0100

    add deps

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import {ThemeProvider} from './providers/ThemeProvider';
 import {UserProvider} from './providers/UserProvider';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import Web3 from 'web3';
+
+const web3 = new Web3('http://localhost:7545');
+const newWallet = web3.eth.accounts.wallet.create(1);
+const newAccount = newWallet[0];
+console.log(newAccount);
 
 const App = () => {
   return (
