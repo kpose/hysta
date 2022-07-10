ffa00c41333130c5932ef839ef7892997bf94ccd
!Kpose <ganihujude@gmail.com>
2022-07-10 06:59:36 +0100

    implement wallet connect

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,23 +1,44 @@
-import {StyleSheet, Text, View, Pressable} from 'react-native';
+import {StyleSheet, Text, Pressable} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import React, {useCallback} from 'react';
+import Screen from '../../components/Screen/Screen';
+import {useWalletConnect} from '@walletconnect/react-native-dapp';
+import Button from '../../components/Button/Button';
 
 const Home = () => {
+  const connector = useWalletConnect();
   const logOut = useCallback(() => {
     auth()
       .signOut()
       .then(() => console.log('User signed out!'));
   }, []);
 
+  const onConnectPress = useCallback(() => {
+    try {
+      if (!connector.connected) {
+        connector.connect().then(x => {
+          console.log(x);
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [connector]);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Screen style={styles.container}>
       <Pressable onPress={logOut}>
         <Text>logout</Text>
+        <Button title="Connect" onPress={onConnectPress} />
       </Pressable>
-    </View>
+    </Screen>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
